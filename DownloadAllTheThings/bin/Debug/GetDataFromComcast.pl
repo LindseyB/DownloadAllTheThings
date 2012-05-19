c0878771b15e11f6d11d541d02a6b3c888a953ef
LindseyB <hazardous.narayan@gmail.com>
2012-05-19 14:21:47 -0400

    Update the perl script to use WWW::Scripter for JS.
    Another attempt to get the javascript pages to load. This one uses WWW::Scripter and the JavaScript plugin. Unfortunately it appears to be erroring out after the form submit. It seems to do fine on the first page of JavaScript however.

--- DownloadAllTheThings/bin/Debug/GetDataFromComcast.pl
+++ DownloadAllTheThings/bin/Debug/GetDataFromComcast.pl
@@ -1,27 +1,26 @@
 use strict;
-use WWW::Mechanize;
+use WWW::Scripter;
 
-my $mech = WWW::Mechanize->new();
+my $script = new WWW::Scripter;
+$script->use_plugin('JavaScript');
 
 my $username = $ARGV[0];
 my $password = $ARGV[1]; 
 
-
 my $url = 'https://customer.comcast.com/Secure/UsageMeterDetail.aspx';
 
-$mech->get($url);
+$script->get($url);
 
-$mech->submit_form(
+$script->submit_form(
     form_name => 'signin',
     fields      => {
         user    => $username,
         passwd  => $password,
-        check	=> 1,
     }
 );
 
-$mech->submit_form( form_name => 'redir' );
-$mech->submit_form( form_name => 'aspnetForm');
+#$mech->submit_form( form_name => 'aspnetForm');
 
 # some reason this is the loading text rather than the correct text :<
-print $mech->content();
+print $script->text();
+
