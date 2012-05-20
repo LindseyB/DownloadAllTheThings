eab3d389bdfbd8ebc52b444394335d5ca0b50a62
LindseyB <hazardous.narayan@gmail.com>
2012-05-20 03:08:42 -0400

    Proof of concept bitches!!!
     It needs to be cleaned up, but it actually does work. There are two page loads that happen between entering the login details and actually hitting the page with the information I want. This was the key fact. Once I understood this and found some really nice example code (http://snippets.aktagon.com/snippets/534-How-to-scrape-web-pages-with-PhantomJS-and-jQuery) it went beautifully.

--- DownloadAllTheThings/bin/Debug/GetDataFromComcast.js
+++ DownloadAllTheThings/bin/Debug/GetDataFromComcast.js
@@ -1,29 +1,79 @@
-var system = require('system');
-var page = require('webpage').create();
-var url = 'https://customer.comcast.com/Secure/UsageMeterDetail.aspx';
-
-
-var username = system.args[1];
-var password = system.args[2];
+var system = require('system'),
+	page = require('webpage').create(),
+	url = 'https://customer.comcast.com/Secure/UsageMeterDetail.aspx',
+	stepIndex = 1,
+	username = system.args[1],
+	password = system.args[2];
 
 page.onConsoleMessage = function (msg) {
-    console.log(msg);
+	// for debugging, remove the console> later
+    console.log('console>' + msg);
+};
+
+// for debugging, just in case
+page.onAlert = function (msg) {
+	console.log('alert>' + msg);
 };
 
 
 page.open(url, function (status) {
-	if (status !== 'success') {
-		console.log('error loading page');
-	} else {
-		//page is open, now do a thing
-		page.evaluate(function () {
-			document.getElementById('user').value = window.username;
-			document.getElementById('passwd').value = window.password;
-			document.forms['signin'].submit();
-			console.log('submitting form...');
-			console.log(document.body.innerHTML);
-		});
+	if (status === 'success') {
+		console.log('=======================================');
+		console.log('Step ' + stepIndex);
+		console.log('=======================================');
+
+		// let's get some jquery up in this bitch
+		page.injectJs('jquery-1.7.2.min.js');
+
+		if(!phantom.state){
+			initialize();
+		} else {
+			phantom.state();
+		}
+
+		// save a screenshot - because otherwise I won't know wtf
+		page.render('step' + stepIndex++ + ".png");
 	}
-	phantom.exit();
 });
+
+// Step 1
+function initialize() {
+	page.evaluate(function() {
+		document.getElementById('user').value = 'hardcodedusername';
+		document.getElementById('passwd').value = 'hardcodedpassword';
+		document.forms['signin'].submit();
+		console.log('submitting form...');
+	});
+
+	phantom.state = waitForLoad;
+}
+
+// Step 2
+function waitForLoad() {
+	page.evaluate(function() {
+		console.log(document.getElementById('loadingMessage').innerHTML);
+	});
+
+	phantom.state = waitForLoad2;
+}
+
+//step 3
+function waitForLoad2() {
+	page.evaluate(function() {
+		console.log(document.getElementById('loadingMessage').innerHTML);
+	});
+
+	phantom.state = scrapeData;	
+}
+
+// Step 4
+function scrapeData() {
+	page.evaluate(function() {
+		// to do: grab the data
+		//console.log(document.body.innerHTML);
+		console.log(document.getElementById('ctl00_ctl00_ContentArea_PrimaryColumnContent_UsedWrapper').innerHTML);
+	});
+
+	phantom.exit();
+}
 
