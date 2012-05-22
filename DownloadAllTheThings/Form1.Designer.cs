748a1847f7f9cbe195c9b59cd7c54f643497cdee
LindseyB <hazardous.narayan@gmail.com>
2012-05-22 16:28:54 -0400

    Updated the icon.
    Updated the icon to be more simplistic and therefore look better when it is in the system tray.

--- DownloadAllTheThings/Form1.Designer.cs
+++ DownloadAllTheThings/Form1.Designer.cs
@@ -28,84 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.usernameLabel = new System.Windows.Forms.Label();
-            this.passwordLabel = new System.Windows.Forms.Label();
-            this.usernameBox = new System.Windows.Forms.TextBox();
-            this.passwordBox = new System.Windows.Forms.TextBox();
-            this.saveButton = new System.Windows.Forms.Button();
-            this.downloadIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.SuspendLayout();
-            // 
-            // usernameLabel
-            // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(13, 13);
-            this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(56, 13);
-            this.usernameLabel.TabIndex = 0;
-            this.usernameLabel.Text = "username:";
-            // 
-            // passwordLabel
-            // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(14, 42);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(55, 13);
-            this.passwordLabel.TabIndex = 1;
-            this.passwordLabel.Text = "password:";
-            // 
-            // usernameBox
-            // 
-            this.usernameBox.Location = new System.Drawing.Point(74, 13);
-            this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(231, 20);
-            this.usernameBox.TabIndex = 2;
-            // 
-            // passwordBox
-            // 
-            this.passwordBox.Location = new System.Drawing.Point(74, 39);
-            this.passwordBox.Name = "passwordBox";
-            this.passwordBox.Size = new System.Drawing.Size(231, 20);
-            this.passwordBox.TabIndex = 3;
-            // 
-            // saveButton
-            // 
-            this.saveButton.Location = new System.Drawing.Point(230, 66);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(75, 23);
-            this.saveButton.TabIndex = 4;
-            this.saveButton.Text = "save";
-            this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
-            // 
-            // downloadIcon
-            // 
-            this.downloadIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("downloadIcon.Icon")));
-            this.downloadIcon.Text = "Download All The Things";
-            this.downloadIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.downloadIcon_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(317, 101);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.passwordBox);
-            this.Controls.Add(this.usernameBox);
-            this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.usernameLabel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.Name = "Form1";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "Download All The Things";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.usernameLabel = new System.Windows.Forms.Label();
+			this.passwordLabel = new System.Windows.Forms.Label();
+			this.usernameBox = new System.Windows.Forms.TextBox();
+			this.passwordBox = new System.Windows.Forms.TextBox();
+			this.saveButton = new System.Windows.Forms.Button();
+			this.downloadIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.SuspendLayout();
+			// 
+			// usernameLabel
+			// 
+			this.usernameLabel.AutoSize = true;
+			this.usernameLabel.Location = new System.Drawing.Point(13, 13);
+			this.usernameLabel.Name = "usernameLabel";
+			this.usernameLabel.Size = new System.Drawing.Size(56, 13);
+			this.usernameLabel.TabIndex = 0;
+			this.usernameLabel.Text = "username:";
+			// 
+			// passwordLabel
+			// 
+			this.passwordLabel.AutoSize = true;
+			this.passwordLabel.Location = new System.Drawing.Point(14, 42);
+			this.passwordLabel.Name = "passwordLabel";
+			this.passwordLabel.Size = new System.Drawing.Size(55, 13);
+			this.passwordLabel.TabIndex = 1;
+			this.passwordLabel.Text = "password:";
+			// 
+			// usernameBox
+			// 
+			this.usernameBox.Location = new System.Drawing.Point(74, 13);
+			this.usernameBox.Name = "usernameBox";
+			this.usernameBox.Size = new System.Drawing.Size(231, 20);
+			this.usernameBox.TabIndex = 2;
+			// 
+			// passwordBox
+			// 
+			this.passwordBox.Location = new System.Drawing.Point(74, 39);
+			this.passwordBox.Name = "passwordBox";
+			this.passwordBox.Size = new System.Drawing.Size(231, 20);
+			this.passwordBox.TabIndex = 3;
+			// 
+			// saveButton
+			// 
+			this.saveButton.Location = new System.Drawing.Point(230, 66);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(75, 23);
+			this.saveButton.TabIndex = 4;
+			this.saveButton.Text = "save";
+			this.saveButton.UseVisualStyleBackColor = true;
+			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// downloadIcon
+			// 
+			this.downloadIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("downloadIcon.Icon")));
+			this.downloadIcon.Text = "Download All The Things";
+			this.downloadIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.downloadIcon_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(317, 101);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.passwordBox);
+			this.Controls.Add(this.usernameBox);
+			this.Controls.Add(this.passwordLabel);
+			this.Controls.Add(this.usernameLabel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "Download All The Things";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
